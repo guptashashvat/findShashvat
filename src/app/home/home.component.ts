a454dfc481d2c461562c8a1ec5e76bf9ba10ec6f
Shashvat Gupta <shashvat.gupta@newgen.co.in>
2019-09-26 23:01:23 +0530

    Commiting begin of my website

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
+   title = 'ShashvatArena';
 
   ngOnInit() {
   }
