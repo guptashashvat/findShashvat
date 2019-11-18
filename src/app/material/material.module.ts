20c7e8318ee083e9b67b0d7b16c97257e82520b0
shashvatgupta <shashvatgupta39@gmail.com>
2019-11-19 00:14:36 +0530

    ok

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule, MatButtonModule } from '@angular/material';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 
@@ -9,10 +10,12 @@ import { MatDialogModule, MatButtonModule } from '@angular/material';
   imports: [
     CommonModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressBarModule
   ],
   exports: [
     MatDialogModule,
-    MatButtonModule]
+    MatButtonModule,
+    MatProgressBarModule]
 })
 export class MaterialModule { }
