a923f7f56e630df60399c9f2bee78298c5691f32
shashvatgupta <shashvatgupta39@gmail.com>
2020-02-11 00:08:19 +0530

    After trying to publish

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Contact } from '../contactDetails';
 import { EmailClientService } from '../email-client.service';
 import '../../assets/smtpJS.js';
-
+declare let Email: any;
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -10,23 +10,22 @@ import '../../assets/smtpJS.js';
 })
 export class ContactComponent implements OnInit {
   contact: Contact=new Contact();
-  constructor(private emailService: EmailClientService) { };
-  Email : any;
+  constructor(private emailService: EmailClientService) { }; 
 
   ngOnInit() {
   }
   processForm(){
-    this.Email.send({
-      Host : "smtp.yourisp.com",
-      Username : "username",
-      Password : "password",
-      To : 'them@website.com',
-      From : "you@isp.com",
+   Email.send({
+      Host : "smtp.gmail.com",
+      Username : "shashvatgupta39@gmail.com",
+      Password : "Dhinkachika39#",
+      To : 'shashvatgupta39@gmail.com',
+      From : "shashvatgupta39@gmail.com",
       Subject : "This is the subject",
       Body : "And this is the body"
   }).then(
-    message => alert(message));
-    this.emailService.sendMail(this.contact).subscribe(res=> console.log("res: "+res));
+    message => alert(message+" Test"));
+    //this.emailService.sendMail(this.contact).subscribe(res=> console.log("res: "+res));
     const allInfo=`My name is ${this.contact.name}. My email is ${this.contact.email}. My phone number is ${this.contact.phone}. My message is ${this.contact.message}`;
     alert(allInfo);
   }
