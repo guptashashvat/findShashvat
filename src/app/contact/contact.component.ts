70f505b03e149849cda06248632c4dbc04445e72
Shashvat Gupta <shashvat.gupta@newgen.co.in>
2020-02-10 20:21:16 +0530

    Trying email functionality without server

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Contact } from '../contactDetails';
-import { EmailClientService } from '../email-client.service'
+import { EmailClientService } from '../email-client.service';
+import '../../assets/smtpJS.js';
 
 @Component({
   selector: 'app-contact',
@@ -8,13 +9,23 @@ import { EmailClientService } from '../email-client.service'
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
   contact: Contact=new Contact();
-  constructor(private emailService: EmailClientService) { }
+  constructor(private emailService: EmailClientService) { };
+  Email : any;
 
   ngOnInit() {
   }
   processForm(){
+    this.Email.send({
+      Host : "smtp.yourisp.com",
+      Username : "username",
+      Password : "password",
+      To : 'them@website.com',
+      From : "you@isp.com",
+      Subject : "This is the subject",
+      Body : "And this is the body"
+  }).then(
+    message => alert(message));
     this.emailService.sendMail(this.contact).subscribe(res=> console.log("res: "+res));
     const allInfo=`My name is ${this.contact.name}. My email is ${this.contact.email}. My phone number is ${this.contact.phone}. My message is ${this.contact.message}`;
     alert(allInfo);
